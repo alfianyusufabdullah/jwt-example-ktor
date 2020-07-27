67b939c7a2c58455bf0d82661736da513a8925f0
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-07-27 23:47:34 +0700

    add jwt principle classes

---
+++ src/entity/AuthPrinciple.kt
@@ -0,0 +1,5 @@
+package alfianyusufabdullah.ktor.jwt.entity
+
+import io.ktor.auth.Principal
+
+class AuthPrinciple(val user: User) : Principal
---
+++ src/entity/User.kt
@@ -0,0 +1,3 @@
+package alfianyusufabdullah.ktor.jwt.entity
+
+data class User(val username: String, val password: String)
