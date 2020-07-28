ae0b01fd87886792fbc71b31c8e62585ac1e06f3
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-07-28 10:03:07 +0700

    added token expired

--- src/common/AuthenticationConfig.kt
+++ src/common/AuthenticationConfig.kt
@@ -4,10 +4,12 @@ import alfianyusufabdullah.ktor.jwt.entity.User
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import java.util.*
 
 object AuthenticationConfig {
     private const val secret = "alfianyusufabdullah.ktor.example.SECRET"
     private const val issuer = "alfianyusufabdullah.ktor.example.ISSUER"
+    private const val validityInMs = 36_000_00 * 24 * 7
 
     val verifier: JWTVerifier = JWT
         .require(Algorithm.HMAC256(secret))
@@ -21,7 +23,10 @@ object AuthenticationConfig {
             .withClaim("username", user?.username)
             .withClaim("password", user?.password)
             .withClaim("time", System.currentTimeMillis())
+            .withExpiresAt(getExpiration())
             .sign(Algorithm.HMAC256(secret))
     }
+
+    private fun getExpiration() = Date(System.currentTimeMillis() + validityInMs)
 
 }
