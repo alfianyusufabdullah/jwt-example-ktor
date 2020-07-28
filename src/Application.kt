ff8447edb274c3988b079c534a1836bff1012801
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-07-28 12:43:17 +0700

    added exception when unauthorized

--- src/Application.kt
+++ src/Application.kt
@@ -3,21 +3,25 @@ package alfianyusufabdullah.ktor.jwt
 import alfianyusufabdullah.ktor.jwt.common.AuthenticationConfig
 import alfianyusufabdullah.ktor.jwt.entity.AuthPrinciple
 import alfianyusufabdullah.ktor.jwt.entity.User
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.auth.authenticate
 import io.ktor.auth.authentication
 import io.ktor.auth.jwt.jwt
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.gson.*
-import io.ktor.features.*
-import io.ktor.client.*
-import io.ktor.client.engine.jetty.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveOrNull
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
 import io.ktor.util.KtorExperimentalAPI
-import org.eclipse.jetty.util.log.Log
+import java.util.*
 
 val ApplicationCall.authPrinciple get() = authentication.principal<AuthPrinciple>()
 
@@ -34,6 +38,17 @@ fun Application.module() {
         }
     }
 
+    install(StatusPages){
+        status(HttpStatusCode.Unauthorized){
+            call.respond(
+                HttpStatusCode.Unauthorized,
+                mapOf(
+                    "message" to "Unauthorized"
+                )
+            )
+        }
+    }
+
     install(Authentication) {
         jwt {
             realm = jwtRealm
@@ -41,10 +56,11 @@ fun Application.module() {
             validate {
                 val name = it.payload.getClaim("username").asString()
                 val password = it.payload.getClaim("password").asString()
+                val expired = it.payload.getClaim("expired").asLong()
 
                 if (name != null && password != null) {
                     val user = User(name, password)
-                    AuthPrinciple(user)
+                    AuthPrinciple(user, expired)
                 } else {
                     null
                 }
@@ -54,19 +70,25 @@ fun Application.module() {
 
     routing {
         authenticate {
+            get("/expired") {
+                call.authPrinciple?.let {
+                    val date = Date(it.expired)
+                    call.respond(
+                        HttpStatusCode.OK,
+                        mapOf(
+                            "message" to "success!",
+                            "expired_at" to date
+                        )
+                    )
+                }
+            }
+
             get("/test/request") {
                 call.authPrinciple?.let {
                     call.respond(
                         HttpStatusCode.OK,
                         mapOf(
                             "message" to "Success! getting new data.."
-                        )
-                    )
-                } ?: kotlin.run {
-                    call.respond(
-                        HttpStatusCode.Unauthorized,
-                        mapOf(
-                            "message" to "error getting information, be sure token included"
                         )
                     )
                 }
@@ -81,8 +103,9 @@ fun Application.module() {
                     HttpStatusCode.OK,
                     mapOf(
                         "message" to "Success! Don't tell anyone about your token",
-                        "time" to System.currentTimeMillis(),
-                        "token" to newToken
+                        "created_at" to newToken["created_at"],
+                        "expired_at" to newToken["expired_at"],
+                        "token" to newToken["token"]
                     )
                 )
             } ?: kotlin.run {
